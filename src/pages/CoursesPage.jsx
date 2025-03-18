7ff1ec42c9f610d86d56126aa9a8a20d35d28bd7
AmirEsfandiar <amiresfandiar2000@gmail.com>
2025-03-18 15:40:35 +0330

    initial project

---
+++ src/pages/CoursesPage.jsx
@@ -0,0 +1,25 @@
+import { Outlet } from "react-router-dom";
+
+// there is no need for this component to have a children route , just have this your mind...
+
+function CoursesPage() {
+  return (
+    <div>
+      <Outlet />
+      <div className="main-courses border mx-[auto] gap-[10px] w-[95%] flex flex-wrap flex-row justify-center">
+        <div className="top-filter w-[100%] bg-[#ccc] h-[50px] rounded-lg mt-[20px]">
+          TOP-FILTER
+        </div>
+        <div className="main-courses-slider border mt-[10px] w-[75%] h-[500px] rounded-md">
+          MAIN-COURSES-SLIDER
+        </div>
+        <div className="right-filter border mt-[10px] w-[23%] h-[200px]  rounded-md"></div>
+      </div>
+      <div className="course-slider border mx-[auto] w-[99%] h-[100px]">
+        course-slider
+      </div>
+    </div>
+  );
+}
+
+export default CoursesPage;
