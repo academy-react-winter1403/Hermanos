c422cf238bde887597e97be93da4a8b3cdeb827f
AmirEsfandiar <amiresfandiar2000@gmail.com>
2025-03-18 17:26:15 +0330

    starting to implementing courses page

---
+++ src/components/coursesPage-components/CoursesFilter.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function CoursesFilter() {
+  return (
+    <div className="right-filter border mt-[10px] w-[23%] h-[200px]  rounded-md"></div>
+  );
+}
+
+export default CoursesFilter;
