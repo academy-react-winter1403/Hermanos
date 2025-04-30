f8a5d3a1c8e000fa4f0bf3bdc8e058e40613b43a
AmirEsfandiar <amiresfandiar2000@gmail.com>
2025-04-30 12:08:04 +0330

    ...

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -25,7 +25,7 @@ function Header() {
   else
     return (
       <>
-        <div className="header-container text-[rgba(34,68,93,1)] w-[100%] h-[80px] flex flex-nowrap flex-row items-center justify-between bg-linear-to-br from-[rgba(164,246,222,1)] to-[rgba(255,255,255,1)]">
+        <div className="header-container text-[rgba(34,68,93,1)] w-[100%] h-[80px] flex flex-nowrap flex-row items-center justify-between">
           <HeaderSignInBtn />
           <HeaderNavigations />
           <Logo />
